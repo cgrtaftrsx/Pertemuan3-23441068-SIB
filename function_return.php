47075a6ccc7b01ff4aa88a5e3303d8c434016015
nursitihazarazwati <162719646+cgrtaftrsx@users.noreply.github.com>
2024-11-01 15:40:41 +0700

    Add files via upload

---
+++ function_return.php
@@ -0,0 +1,8 @@
+<?php
+function tambah($a, $b)
+{
+    $hasil = $a, $b;
+    return $hasil;
+}
+$jumlah = tambah(2, 3); // Memanggil fungsi dan menyimpan hasilnya dalam variabel $jumlah
+?>
---
+++ parameter.php
@@ -0,0 +1,7 @@
+<?php
+function tambah($a, $b){
+    $hasil = $a + $b;
+    echo $hasil;
+}
+tambah(2, 3); // Memanggil fungsi tambah dengan parameter 2 dan 3
+?>
